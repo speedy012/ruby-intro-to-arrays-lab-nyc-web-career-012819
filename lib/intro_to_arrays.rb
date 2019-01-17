f0bdde96b457df7da2ad23bddc650d922f992859
Herman <hrivas5726@aol.com>
2019-01-17 04:11:38 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -8,6 +8,6 @@ end
 
 def first_element(array, element)
   @taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
-  @taylor_swift[0]
+   my_first_element = first_element(@taylor_swift)
 end
   
