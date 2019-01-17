a8df079d4de5bfd9aef21b733c3dd138eae89b01
Herman <hrivas5726@aol.com>
2019-01-17 04:39:25 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -21,7 +21,7 @@ def last_element(my_last_element)
     my_last_element = @taylor_swift[-1]
 end
 
-def first_element_with_arrays_methods(first_country)
+def first_element_with_array_methods(first_country)
   @south_east_asia = ["Thailand", "Cambodia", "Singapore", "Myanmar"]
   first_country = @south_east_asia.first
 end
